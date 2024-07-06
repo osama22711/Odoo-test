181d18e008cd0096c81ce1278d1547ba5d31826f
Osama albawab <42546204+osama22711@users.noreply.github.com>
2024-07-06 13:30:44 +0300

    First push of Odoo test repo

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Hopsital Management System",
+    "author": "Osama Albawab",
+    "licence": "LGPL-3",
+    "version": "1.0.0",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/menu.xml",
+        "views/patient_views.xml"
+    ]
+}
